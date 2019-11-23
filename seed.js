4218e3e01e6467b5e150dee44631c5f93f92bf14
Emily Kwak <emilykwak1@gmail.com>
2019-11-23 13:04:22 -0800

    refactored data generator

--- seed.js
+++ seed.js
@@ -1,46 +1,67 @@
 const faker = require('faker');
+const _ = require("lodash");
 
 // {
-//   memberId: String(“m” + Number),
-//   avatar: String(URL),
-//   thumbnail: String(URL),
-//   name: String,
-//   events: [
-//     {
-//       eventId: Number,
-//       eventOrganizer: Boolean,
-//       status: Boolean,
-//       favorite: Boolean
-//     }
-//   ]
+//   memberId: String('m' + Number)
+//   name: String
+//   avatar: String(URL)
+//   thumbnail: String(URL)
+//   eventOrganizer: Boolean
+//   favorite: Boolean
 // }
 
+// {
+//   eventId: Number
+//   limit: Boolean
+//   setLimit: Number / NULL
+//   attendees: Array
+//   waitlist: Array
+// }
+
+
+
+// Attendees
 let attendees = [];
 
-for (let i = 0; i <= 499; i++) {
+for (let i = 0; i < 499; i++) {
   const memberId = `m${i}`;
-  const avatar = 'http://placecorgi.com/50';
-  const thumbnail = 'http://placecorgi.com/100';
   const name = faker.name.findName();
-  const eventId = Math.floor(Math.random() * 100);
-  const eventOrganizer = faker.random.boolean();
-  const status = faker.random.boolean();
+  const avatar = 'http://placecorgi.com/100';
+  const thumbnail = 'http://placecorgi.com/50';
   const favorite = faker.random.boolean();
 
-  let newUser = {
+  let newAttendee = {
     memberId,
+    name,
     avatar,
     thumbnail,
-    name,
-    events: [
-      {
-        eventId,
-        eventOrganizer,
-        status,
-        favorite
-      }
-    ]
+    favorite
+  }
+
+  attendees.push(newAttendee)
+}
+
+
+// Events
+let events = [];
+
+for (let i = 0; i < 100; i++) {
+  const eventId = i;
+  const limit = faker.random.boolean();
+  const setLimit = limit ? faker.random.number({ min: 20, max: 100 }) : null;
+  const randomNum = faker.random.number({ min: 1, max: 100 });
+  const attendees = _.sampleSize(attendees, randomNum);
+  const eventOrganizer = faker.random.number({ min: 1, max: 2 })
+  const waitlist = [];
+
+  let newEvents = {
+    eventId,
+    limit,
+    setLimit,
+    attendees,
+    eventOrganizer,
+    waitlist,
   };
 
-  attendees.push(newUser);
+  events.push(newEvents);
 }
