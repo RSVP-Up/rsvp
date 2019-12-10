f0839e672d48c8513d300b1905474aa7ef21ccb2
Emily Kwak <emilykwak1@gmail.com>
2019-12-09 18:38:43 -0800

    check for correct event object keys

--- tests/seed.test.js
+++ tests/seed.test.js
@@ -1,7 +1,24 @@
+const _ = require('lodash');
 const events = require('../database/seed.js');
 
 describe('Event', () => {
   test('Should have generated 100 events', () => {
-    expect(events).toHaveLength(100)
+    expect(events).toHaveLength(100);
+  });
+  test('Event ID data should be a number', () => {
+    _.forEach(events, (event) => {
+      expect(event).toEqual(expect.objectContaining({
+        eventId: expect.any(Number),
+      }))
+    })
   })
-})
+});
+
+describe('Event Schema', () => {
+  test('Should return an event object with the following keys: eventId, limit, setLimit, attendees, eventOrganizer, waitlist', () => {
+    _.forEach(events, (event) => {
+      expect(Object.keys(event)).toContain('eventId', 'limit', 'setLimit', 'attendees', 'eventOrganizer', 'waitlist')
+    })
+  });
+});
+
