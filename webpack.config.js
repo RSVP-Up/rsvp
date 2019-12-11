452a4f9f0d7f1e0c2ca389ff2997f62762dc2df4
Emily Kwak <emilykwak1@gmail.com>
2019-12-10 20:35:21 -0800

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var PUB_DIR = path.join(__dirname, '/public');
-
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: PUB_DIR
-  },
+  entry: __dirname + '/client/src/index.jsx',
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: 'react'
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
         }
       }
     ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public'
   }
 };
