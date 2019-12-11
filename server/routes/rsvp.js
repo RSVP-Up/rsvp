18ca50679730d62f454f1eebbf2ac9bee2c66f13
Emily Kwak <emilykwak1@gmail.com>
2019-12-10 19:11:46 -0800

    remove unused routes and send eventHost and attendees

--- server/routes/rsvp.js
+++ server/routes/rsvp.js
@@ -1,12 +1,6 @@
 const router = require('express').Router();
 let Event = require('../../database/models/Event.js');
 
-router.route('/').get((req, res) => {
-  Event.find()
-    .then(events => res.json(events))
-    .catch(err => res.status(400).json(`Error: ${err}`));
-})
-
 router.route('/:eventId').get((req, res) => {
   Event.findOne({ eventId: req.params.eventId }, 'attendees waitlist')
     .then((attendeesCount) => res.json(attendeesCount))
@@ -24,24 +18,7 @@ router.route('/hosts/:eventId').get((req, res) => {
         }
         eventHosts.push(person)
       })
-      res.json({ eventHosts })
-    })
-    .catch(err => res.status(400).json(`Error: ${err}`));
-})
-
-router.route('/profiles/:eventId').get((req, res) => {
-  Event.findOne({ eventId: req.params.eventId })
-    .then((event) => {
-      var attendees = []
-      event.attendees.forEach(user => {
-        const person = {
-          memberId: user.memberId,
-          name: user.name,
-          thumbnail: user.thumbnail
-        }
-        attendees.push(person)
-      })
-      res.json({ attendees })
+      res.json(eventHosts)
     })
     .catch(err => res.status(400).json(`Error: ${err}`));
 })
@@ -57,7 +34,7 @@ router.route('/attendees/:eventId').get((req, res) => {
         }
         attendees.push(person)
       })
-      res.json({ attendees })
+      res.json(attendees)
     })
     .catch(err => res.status(400).json(`Error: ${err}`));
 })
