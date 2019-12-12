c453d1f5305c6a044710a6058756ba4ec0ed2103
Emily Kwak <emilykwak1@gmail.com>
2019-12-11 19:06:55 -0800

    render RSVP component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import RSVP from './components/RSVP.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<RSVP />, document.getElementById('app'));
