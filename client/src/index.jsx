846c24232e4666c7226f249f326edd830601d5eb
Emily Kwak <emilykwak1@gmail.com>
2019-12-12 19:22:19 -0800

    pass sample event data as props to rsvp component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RSVP from './components/RSVP.jsx';
 
-ReactDOM.render(<RSVP />, document.getElementById('app'));
+var event = {
+  "title": "Grass-roots web-enabled info-mediaries",
+  "local_date_time": "2019-10-07T08:05:25.286Z",
+  "org_name": "Koch, Blick and Turcotte",
+  "private": true
+}
+
+ReactDOM.render(<RSVP event={event} />, document.getElementById('app'));
