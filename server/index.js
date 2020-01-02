50c3fab6980d2cb2f5a499a369b8b88d1c81ba76
Emily Kwak <emilykwak1@gmail.com>
2020-01-02 14:02:43 -0800

    use cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3001;
 const db = require('../database/index.js')
 const rsvpRouter = require('./routes/rsvp');
 
+app.use(cors());
 app.use(express.static('public'));
 
 app.use('/rsvp', rsvpRouter);
